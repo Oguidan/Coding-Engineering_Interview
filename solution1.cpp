8ded1f2211ba7545e0523d1f4cd70d5278e5c6fe
Michael Oguidan <michaeloguidan@gmail.com>
2024-05-22 10:47:25 +0000

    comment solution1

--- solution1.cpp
+++ solution1.cpp
@@ -1,8 +1,9 @@
 #include <vector>
 using namespace std;
 bool HasPairWithSum(const vector<int>& data, int sum) {
-    int low = 0;
-    int hi = data.size() - 1;
+    int low = 0; // lowest index in the vector data
+    int hi = data.size() - 1; // highest index in the vector data
+    /*If the tow index cross each other and and none right sum values is find the program end without finding any value*/
     while (low < hi) {
         int s = data[low] + data[hi];
         if (s == sum) return true;
